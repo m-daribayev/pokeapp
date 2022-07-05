42eb8d92ac354f0c18fe9f72d8ad396b6ffe5ea9
Margulan Daribayev <m-daribayev@mercari.com>
2022-07-05 15:15:42 +0900

    Add first project setup

---
+++ PokeApp/PokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAppApp.swift
+//  PokeApp
+//
+//  Created by m-daribayev on 2022/07/05.
+//
+
+import SwiftUI
+
+@main
+struct PokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
